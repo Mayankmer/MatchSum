f6f59eed1139c3df32786b745da4cc0c5285ccab
Mayankmer <mayankmer24@gmail.com>
2025-02-17 00:42:49 +0530

    edited dataloader

--- dataloader.py
+++ dataloader.py
@@ -1,92 +1,92 @@
 from time import time
 from datetime import timedelta
+import json
 
 from fastNLP.io.loader import JsonLoader
 from fastNLP.io.data_bundle import DataBundle
 from fastNLP.io.pipe.pipe import Pipe
 from fastNLP.core.const import Const
+from fastNLP import DataSet, Instance
+from transformers import AutoTokenizer
 
 class MatchSumLoader(JsonLoader):
-    
     def __init__(self, candidate_num, encoder, max_len=180):
-        fields = {'text_id': 'text_id',
-             'candidate_id': 'candidate_id',
-               'summary_id': 'summary_id'
-                 }
-        super(MatchSumLoader, self).__init__(fields=fields)
-        
+        super(MatchSumLoader, self).__init__()
         self.candidate_num = candidate_num
         self.max_len = max_len
         self.encoder = encoder
-
-        if encoder == 'bert':
-            self.sep_id = [102] # '[SEP]' (BERT)
-        else:
-            self.sep_id = [2] # '</s>' (RoBERTa)
+        
+        self.tokenizer = AutoTokenizer.from_pretrained(encoder)
+        self.sep_token_id = self.tokenizer.sep_token_id
+        self.pad_token_id = self.tokenizer.pad_token_id
 
     def _load(self, path):
-        dataset = super(MatchSumLoader, self)._load(path)
-        return dataset
-    
-    def load(self, paths):        
+        data = []
+        with open(path, "r") as f:
+            for line in f:
+                example = json.loads(line)
+                src = example["src"]
+                candidates = example["candidates"][:self.candidate_num]
+                
+                # Tokenize and pad article
+                src_ids = self.tokenizer.encode(
+                    src, 
+                    max_length=self.max_len,
+                    truncation=True,
+                    padding='max_length',
+                    add_special_tokens=True
+                )
+                
+                # Tokenize and pad candidates
+                candidate_ids = []
+                for cand in candidates:
+                    cand_ids = self.tokenizer.encode(
+                        cand, 
+                        max_length=self.max_len,
+                        truncation=True,
+                        padding='max_length',
+                        add_special_tokens=True
+                    )
+                    candidate_ids.append(cand_ids)
+                
+                instance = Instance(
+                    text_id=src_ids,
+                    candidate_id=candidate_ids,
+                    summary_id=candidate_ids[0]
+                )
+                data.append(instance)
         
-        def get_seq_len(instance):
-            return len(instance['text_id'])
-        
-        def sample(instance, candidate_num):
-            candidate_id = instance['candidate_id'][:candidate_num]
-            return candidate_id
-        
-        def truncate_candidate_id(instance, max_len):
-            candidate_id = []
-            for i in range(len(instance['candidate_id'])):
-                if len(instance['candidate_id'][i]) > max_len:
-                    cur_id = instance['candidate_id'][i][:(max_len - 1)]
-                    cur_id += self.sep_id
-                else:
-                    cur_id = instance['candidate_id'][i]
-                candidate_id.append(cur_id)
-            return candidate_id
+        return DataSet(data)
 
+    def load(self, paths):
         print('Start loading datasets !!!')
         start = time()
 
-        # load datasets
         datasets = {}
         for name in paths:
-            datasets[name] = self._load(paths[name])
+            dataset = self._load(paths[name])
             
-            if name == 'train':
-                datasets[name].apply(lambda ins: truncate_candidate_id(ins, self.max_len), new_field_name='candidate_id')
+            dataset.set_input("text_id", "candidate_id")
+            dataset.set_target("summary_id")
             
-            # set input and target
-            datasets[name].set_input('text_id', 'candidate_id', 'summary_id')
-        
-            # set padding value
-            if self.encoder == 'bert':
-                pad_id = 0
-            else:
-                pad_id = 1 # for RoBERTa
-            datasets[name].set_pad_val('text_id', pad_id)
-            datasets[name].set_pad_val('candidate_id', pad_id)
-            datasets[name].set_pad_val('summary_id', pad_id)
+            dataset.set_pad_val("text_id", self.pad_token_id)
+            dataset.set_pad_val("candidate_id", self.pad_token_id)
+            dataset.set_pad_val("summary_id", self.pad_token_id)
             
-        print('Finished in {}'.format(timedelta(seconds=time()-start)))
+            datasets[name] = dataset
 
+        print('Finished in {}'.format(timedelta(seconds=time()-start)))
         return DataBundle(datasets=datasets)
 
 class MatchSumPipe(Pipe):
-
     def __init__(self, candidate_num, encoder):
         super(MatchSumPipe, self).__init__()
         self.candidate_num = candidate_num
         self.encoder = encoder
 
     def process(self, data_bundle):
-
         return data_bundle
         
     def process_from_file(self, paths):
         data_bundle = MatchSumLoader(self.candidate_num, self.encoder).load(paths)
         return self.process(data_bundle)
-
